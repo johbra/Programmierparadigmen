e1d4658335e58aad23df5fa66ea6131ddda9eb8d
Johannes Brauer <johannes@JohMBAir.local>
2017-09-25 17:47:31 +0200

    Aufräumarbeiten / aufgaben.org

--- prolog/darlehen.pl
+++ prolog/darlehen.pl
@@ -1,16 +1,11 @@
-:- use_module(library(clpr)).  
-                                %darlehen(D, T, Z, R, S) :- T=0, D=S. 
-                                %darlehen(D, T, Z, R, S) :- T>0, T1 = T-1, D1 = D + D*Z - R,
-                                %darlehen(D1, T1, Z, R, S).  
+:- use_module(library(clpr)).
 
 darlehen(D, T, Z, R, S) :- {T=0, D=S}.
 darlehen(D, T, Z, R, S) :- {T>0, T1 = T-1, D1 = D + D*Z - R},
         darlehen(D1, T1, Z, R, S).
 
-mortgage(D, T, I, R, S) :-
-        {T = 0,
-         D = S} ;
-        {T > 0,
-         T1 = T - 1},
-        {D1 = D + D*I - R},
-        mortgage(D1, T1, I, R, S).
+                                % Fragen
+                                % darlehen(100000,360,0.01,1025,S).
+                                % darlehen(D,360,0.01,1025,0).
+                                % {S=<0}, darlehen(100000,T,0.01,1025,S). 
+                                % darlehen(D,360,0.01,R,0).
